427957b64d2c30e67650880d33e49432a2b3ea6b
Valentin Bonnamour <valentinbonnamour@gmail.com>
2021-04-04 15:35:15 +0200

    identation + responsive 100% + animation container-profil

--- app.js
+++ app.js
@@ -7,6 +7,10 @@ const goTop = document.querySelector(".goTop");
 const scrollDown = document.querySelector(".scrolldown")
 const totalHeight = document.body.scrollHeight - window.innerHeight;
 
+
+// Partie écoute du scroll de la page :
+
+    // f : Écoute le scroll de la fenètre en cours. La variable ProgressHeight permets de transformer la progression du scroll utilisateur en pourcentage.
 window.addEventListener("scroll", ()=>{
 
     let progressHeight = (window.pageYOffset / totalHeight) * 100;
@@ -21,6 +25,10 @@ window.addEventListener("scroll", ()=>{
     
 })
 
+
+// Partie boutons scoll vers le haut (flèche) :
+
+    // f : Permets de scroller vers le haut de la page au click du bouton ayant la classe 'goTop'.
 goTop.addEventListener('click', ()=>{
     window.scrollTo({
         top: 0,
@@ -28,7 +36,11 @@ goTop.addEventListener('click', ()=>{
         behavior: "smooth"
     })
 })
-// Partie gestion du menu "burger"
+
+
+// Partie gestion du menu "burger" :
+
+    //f : Permets d'enlerver l'overlay du menu quand l'utilisateur clique sur un lien appartenant à celui-ci      
 menuLinks.forEach(links =>{
     links.addEventListener('click', ()=>{
         overlayMenu.classList.remove('open-overlay-menu')
@@ -36,28 +48,29 @@ menuLinks.forEach(links =>{
     })
 })
 
+    //f : Ajoute ou enlève la classe 'open-overlay-menu' sur le menu overlay en fonction du type de bouléen attribué à la variable menuOpen + ajoute la classe 'cross-burger-menu' sur le menu burger qui le change en croix.
 let menuOpen = false;
 burgerMenu.addEventListener('click',()=>{
+    
     if(!menuOpen) {
         overlayMenu.classList.add('open-overlay-menu');
         burgerMenu.classList.add('cross-burger-menu')
+        goTop.style.display="none";
         menuOpen = true;
     }
     else {
         overlayMenu.classList.remove('open-overlay-menu')
         burgerMenu.classList.remove('cross-burger-menu')
         menuOpen = false;
+        goTop.style.display="flex";
     }
     
 })
 
 
+// Partie cards  :
 
-function newFunction() {
-    console.log("Scroll");
-}
-
-// Partie cads (voir plus/voir moins)
+    // f : Gère l'ajoue de la classe 'expand-active', qui permet de voir le contenu de chaque cartes par rapport aux datas html. Cette fonction est appelé via une fonction forEach.
 
 function expand(){
     const showContent = document.getElementById(this.dataset.target);
